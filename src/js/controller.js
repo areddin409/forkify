1589fc525dc7469b99c906def96f66c87460e580
areddin409 <areddin409@gmail.com>
2021-02-05 11:58:17 -0600

    completed recipe results pagination'

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,13 +2,14 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -38,7 +39,11 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (error) {
     console.log(
       '🚀 ~ file: controller.js ~ line 35 ~ controlSearchResults ~ error',
@@ -46,10 +51,18 @@ const controlSearchResults = async function () {
     );
   }
 };
-controlSearchResults();
+
+const controlPagination = function (goToPage) {
+  // 3) Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 4) render new pagination buttons
+  paginationView.render(model.state.search);
+};
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,4 @@
-import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RESULTS_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
@@ -7,6 +6,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RESULTS_PER_PAGE,
   },
 };
 
@@ -36,10 +37,7 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await getJSON(`${API_URL}?search=${query}`);
-    console.log(
-      '🚀 ~ file: model.js ~ line 35 ~ loadSearchResults ~ data',
-      data
-    );
+
     state.search.results = data.data.recipes.map(recipe => {
       return {
         id: recipe.id,
@@ -55,4 +53,13 @@ export const loadSearchResults = async function (query) {
     );
     throw error;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; //0;
+  const end = page * state.search.resultsPerPage; //9;
+
+  return state.search.results.slice(start, end);
 };
